9024a3f537fbefa804c9ab21df0dbd2e8e9ad248
Shashank <s_shashank@outlook.com>
2023-05-16 23:45:53 +0530

    added color mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { Flex, HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const Navbar = () => {
     return (
-        <HStack>
-            <Image src={logo} boxSize="60px" />
-            <Text>GameHub</Text>
-        </HStack>
+        <Flex justify={'space-between'} paddingX={'10px'}>
+            <HStack>
+                <Image src={logo} boxSize="60px" />
+                <Text>GameHub</Text>
+            </HStack>
+            <ColorModeSwitch />
+        </Flex>
     );
 };
 

013f9279ad15d3705a055bc569b11d3f2bb6385f
Shashank <s_shashank@outlook.com>
2023-05-23 22:09:19 +0530

    clean up genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { Button, HStack, Image, List, ListItem } from '@chakra-ui/react';
+import {
+    Button,
+    HStack,
+    Heading,
+    Image,
+    List,
+    ListItem,
+} from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 import GenreListSkeleton from './GenreListSkeleton';
@@ -13,34 +20,41 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
-        <List>
-            {isLoading &&
-                skeletons.map((skeleton) => (
-                    <GenreListSkeleton key={skeleton} />
+        <>
+            <Heading fontSize="2xl" marginBottom={3}>
+                Genres
+            </Heading>
+            <List>
+                {isLoading &&
+                    skeletons.map((skeleton) => (
+                        <GenreListSkeleton key={skeleton} />
+                    ))}
+                {data.map((genre) => (
+                    <ListItem key={genre.id} paddingY="5px">
+                        <HStack>
+                            <Image
+                                boxSize="32px"
+                                borderRadius={8}
+                                src={getCroppedImageUrl(genre.image_background)}
+                                objectFit="cover"
+                            />
+                            <Button
+                                onClick={() => onSelectGenre(genre)}
+                                variant="link"
+                                fontWeight={
+                                    selectedGenre?.id === genre.id
+                                        ? 'bold'
+                                        : 'normal'
+                                }
+                                textAlign="left"
+                            >
+                                {genre.name}
+                            </Button>
+                        </HStack>
+                    </ListItem>
                 ))}
-            {data.map((genre) => (
-                <ListItem key={genre.id} paddingY="5px">
-                    <HStack>
-                        <Image
-                            boxSize="32px"
-                            borderRadius={8}
-                            src={getCroppedImageUrl(genre.image_background)}
-                        />
-                        <Button
-                            onClick={() => onSelectGenre(genre)}
-                            variant="link"
-                            fontWeight={
-                                selectedGenre?.id === genre.id
-                                    ? 'bold'
-                                    : 'normal'
-                            }
-                        >
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+            </List>
+        </>
     );
 };
 

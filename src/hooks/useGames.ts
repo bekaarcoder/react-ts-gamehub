6b22bc8e0dbf105f13e358e162e87e5c6b620823
Shashank <s_shashank@outlook.com>
2023-06-10 22:46:25 +0530

    simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import APIClient, { FetchResponse } from '../services/api-client';
 import { GameQuery } from './../App';
 import { Platform } from './usePlatforms';
+import ms from 'ms';
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -29,6 +30,7 @@ const useGames = (gameQuery: GameQuery) =>
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined;
         },
+        staleTime: ms('6h'),
     });
 
 export default useGames;

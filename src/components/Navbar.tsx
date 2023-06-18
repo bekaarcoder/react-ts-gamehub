0077f000f3540f757d2364627d9acd9e07eacd6d
Shashank <s_shashank@outlook.com>
2023-06-18 16:18:53 +0530

    fixing nav search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,16 @@ import { Flex, HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <HStack paddingX={'10px'} spacing={10}>
             <Flex align="center">
-                <Image src={logo} boxSize="60px" />
-                <Text>GameHub</Text>
+                <Image src={logo} boxSize="60px" objectFit="cover" />
+                <Link to="/">
+                    <Text>GameHub</Text>
+                </Link>
             </Flex>
             <SearchInput />
             <ColorModeSwitch />
